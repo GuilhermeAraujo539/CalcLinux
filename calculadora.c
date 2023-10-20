78f0a06e93d2917c908694186fbd4a2cef9c7ac7
Guilherme de Araujo Souza <guilherme.souza539@etec.sp.gov.br>
2023-10-20 16:56:59 -0300

    psé

---
+++ calculadora.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+	int operacao, opcao;
+	float numero1, numero2, total;
+
+	opcao=1
+	while(opcao==1){
+		printf ("digite um numero:\n);
+		scanf ("%f",&numero1);
+		printf ("digite outro numero:\n");
+		scanf 
