33facea67420514e4cec00b4428dd6744b1afd7a
Guilherme de Araujo Souza <guilherme.souza539@etec.sp.gov.br>
2023-10-23 15:26:41 -0300

    alteracao Vida

--- calculadora.c
+++ calculadora.c
@@ -3,12 +3,44 @@
 
 int main (int argc, char *argv[]){
 
-	int operacao, opcao;
+	int oper, opcao;
 	float numero1, numero2, total;
 
-	opcao=1
-	while(opcao==1){
-		printf ("digite um numero:\n);
-		scanf ("%f",&numero1);
+	opcao=0;
+	while(opcao==0){
+		printf ("digite  o numero correspondente da operacao desejada:\n 1- adicao\n 2- subtracao\n 3- multiplicacao\n 4- divisao\n");
+		scanf ("%d", &oper);
+		printf ("digite um numero:\n");
+		scanf ("%f", &numero1);
 		printf ("digite outro numero:\n");
-		scanf 
+		scanf ("%f", &numero2);
+
+		switch (oper){
+			case 1:
+				total = numero1 + numero2;
+			break;
+
+			case 2:
+				total = numero1 - numero2;
+			break;
+
+			case 3:
+				total = numero1 * numero2;
+			break;
+
+			case 4:
+				total = numero1 / numero2;
+			break;
+
+}
+		printf ("o resultado do seu calculo foi de: %.2f\n", total);
+		printf ("escolha o numero 0 se quiser realizar outro calculo\n digite o numero 1 se quiser sair");
+		scanf ("%d",&opcao);
+
+
+}
+
+return 0;
+
+}
+
